9b0b2ae586a1505c5eea2f272cb9eb5d7a77190a
super12pl <super12@interia.eu>
2023-09-29 17:37:32 +0200

    form handlers

--- java.js
+++ java.js
@@ -2,3 +2,12 @@ zielonepokoje = ["sala wideo","ruchomy pokój","sterownia","pokój z tunelem","k
 żółtepokoje = ["pokój z wirem","cela","ciemny pokój","komora chłodu","komora obrotowa","komora zakłócania","pokój ZRB","pokój luster"]
 czerwonepokoje = ["wanna z kwasem","zalana komora","pokój pułapka","pokój iluzji","komora śmierci", "pokój paranoi","pokój zegarowy","komora pił"]
 niebieskiepokoje = ["pokój centralny","pokój 25","pokój kontrolny"]
+
+document.getElementById("wyborpokoi").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const ilosczielonych = document.getElementById("zieloneinput").value
+    const iloscżółte = document.getElementById("żółteinput").value
+    const iloscczerwonych = document.getElementById("czerwoneinput").value
+  
+    
+  });
