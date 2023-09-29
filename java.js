0b1e2b8be364cdde53cbf3d00092f0b440e2f349
6tab9 <tabolator1@gmail.com>
2023-09-29 17:38:08 +0200

    aaaaaaaaaaa

--- java.js
+++ java.js
@@ -2,3 +2,8 @@ zielonepokoje = ["sala wideo","ruchomy pokój","sterownia","pokój z tunelem","k
 żółtepokoje = ["pokój z wirem","cela","ciemny pokój","komora chłodu","komora obrotowa","komora zakłócania","pokój ZRB","pokój luster"]
 czerwonepokoje = ["wanna z kwasem","zalana komora","pokój pułapka","pokój iluzji","komora śmierci", "pokój paranoi","pokój zegarowy","komora pił"]
 niebieskiepokoje = ["pokój centralny","pokój 25","pokój kontrolny"]
+let ilePokoji = 5
+const pokoje = document.querySelector(".pokoje")
+for(let i = 0; i < ilePokoji; i++){
+    
+}
