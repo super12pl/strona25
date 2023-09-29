080d8d497ff4e3ee3a9f63a27d5f57bbc4126466
super12pl <super12@interia.eu>
2023-09-29 17:16:08 +0200

    pokoje

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
-zielonepokoje = []
-żółtepokoje = []
-czerwonepokoje = []
+zielonepokoje = ["sala wideo","ruchomy pokój","sterownia","pokój z tunelem","komora robotów","komora regeneracji"]
+żółtepokoje = ["pokój z wirem","cela","ciemny pokój","komora chłodu","komora obrotowa","komora zakłócania","pokój ZRB","pokój luster"]
+czerwonepokoje = ["wanna z kwasem","zalana komora","pokój pułapka","pokój iluzji","komora śmierci", "pokój paranoi","pokój zegarowy","komora pił"]
+niebieskiepokoje = ["pokój centralny","pokój 25","pokój kontrolny"]
